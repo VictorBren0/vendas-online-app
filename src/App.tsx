a3c9e0a8b67268bc56758d1f6f95178f7bcc84c5
VictorBren0 <victor.bsr2000@gmail.com>
2023-09-20 12:19:34 -0300

    feat: insert styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import { SafeAreaView, Text } from 'react-native';
+import styled from 'styled-components/native';
+
+const Button = styled.TouchableOpacity`
+  background-color: #ff0000;
+  padding: 10px;
+`;
 
 const App = () => {
   return (
     <SafeAreaView>
       <Text>Testando</Text>
+      <Button>
+        <Text>Testando</Text>
+      </Button>
     </SafeAreaView>
   );
 };
